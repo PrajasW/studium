8ac7d866d562b922f7307bb623e58be3e9b26bff
Prajas <prajaswadekar@gmail.com>
2023-03-18 23:42:15 +0530

    NEW: Linked all webpages

--- studium/myapp/urls.py
+++ studium/myapp/urls.py
@@ -2,7 +2,9 @@ from django.urls import path,include
 from . import views
 
 urlpatterns = [
-    path('',views.home),
-    path('login',views.login),
-    path('signup',views.signup),
+    path('',views.open_home),
+    path('login',views.open_login),
+    path('signup',views.open_signup),
+    path('user',views.user),
+    path('aboutus',views.open_aboutus),
 ]
--- studium/myapp/views.py
+++ studium/myapp/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def open_home(request):
     return render(request,'home.html')
 
-def login(request):
+def open_login(request):
     return render(request,'login.html')
 
-def signup(request):
+def open_signup(request):
     return render(request,'signup.html')
+
+def user(request):
+    return render(request,'user.html')
+
+def open_aboutus(request):
+    return render(request,'aboutus.html')
