98a32d8ddcfeca6ee68f1623b619f661fb374276
Prajas <prajaswadekar@gmail.com>
2023-03-18 23:57:29 +0530

    NEW: Updated settings.py to disable debug

--- studium/studium/settings.py
+++ studium/studium/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$^1h#b%!9eh-9%@9e90knscj8zogim*cfl4vvt^5q6y5k=7^-p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
