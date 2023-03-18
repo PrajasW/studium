ae94c189167b1678c8edc7dfd4fd30c1e2d0973e
Prajas <prajaswadekar@gmail.com>
2023-03-19 01:15:52 +0530

    SNAP: Working code with major bugs

--- studium/studium/settings.py
+++ studium/studium/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-$^1h#b%!9eh-9%@9e90knscj8zogim*cfl4vvt^5q6y5k=7^-p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'myapp.apps.MyappConfig',
+    'chat.apps.ChatConfig',
 ]
 
 MIDDLEWARE = [
